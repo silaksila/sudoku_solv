d0069c3738e24da8df5aceea7968eb1e470f8a5c
silaksila <silaksila27@gmail.com>
2021-02-28 17:46:31 +0100

    k

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import pygame
 
 from button import button
 from img_processing import img_processing
+from solvealg import solve
 
 pygame.init()
 ######################################################################################################
@@ -48,48 +49,6 @@ levels = [
         [6, 0, 0, 0, 0, 0, 5, 7, 2],
     ]
 ]
-
-#####################################################################################################
-# solver algorithm
-
-
-def check(y, x, n):
-    global num
-
-    for i in range(0, 9):
-        if num[y][i] == n:
-            return False
-    for i in range(0, 9):
-        if num[i][x] == n:
-            return False
-    x0 = (x // 3) * 3
-    y0 = (y // 3) * 3
-    for i in range(0, 3):
-        for j in range(0, 3):
-            if num[y0 + i][x0 + j] == n:
-                return False
-    return True
-
-
-def solve():
-    global num, completed_board
-    for y in range(9):
-        for x in range(9):
-            if num[y][x] == 0:
-                for n in range(1, 10):
-                    if check(y, x, n):
-                        num[y][x] = n
-                        solve()
-                        num[y][x] = 0
-                return
-    ###########################################################################################
-    array = np.array(num)
-    with open('solved.txt', 'wb')as f:
-        np.save(f, array)
-
-    with open('solved.txt', 'rb') as f:
-        completed_board = np.load(f).tolist()
-
 
 # GUI
 ################################################################################################################
